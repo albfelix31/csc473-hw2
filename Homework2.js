6db742b733b9c0dc32070e9e5fa80f9f61d74750
albfelix31 <albert.felix31@gmail.com>
2020-10-07 00:21:22 -0400

    added empty/null validation

--- Homework2.js
+++ Homework2.js
@@ -20,17 +20,32 @@ function displayShippingAddress() {
   }
 
   function validate() {
-    var msg= "", fields = document.getElementById("checkout").getElementsByTagName("input");
+    // var msg= "", fields = document.getElementById("checkout").getElementsByTagName("input");
 
-    for (var i=0; i < fields.length; i++){
-      if (fields[i].value == "") 
-        msg += fields[i].title + ' is required. \n';
-    }
+    // for (var i=0; i < fields.length; i++){
+    //   if (fields[i].value == "") 
+    //     msg += fields[i].title + ' is required. \n';
+    // }
 
-    if(msg) {
-      alert(msg);
-      return false;
-    }
-    else
-      return true;
+    // if(msg) {
+    //   alert(msg);
+    //   return false;
+    // }
+    // else
+    //   return true;
+  //   'use strict';
+  //   window.addEventListener('load', function() {
+  //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  //   var forms = document.getElementById("checkout");
+  //   // Loop over them and prevent submission
+  //   var validation = Array.prototype.filter.call(forms, function(form) {
+  //     form.addEventListener('submit', function(event) {
+  //       if (form.checkValidity() === false) {
+  //         event.preventDefault();
+  //         event.stopPropagation();
+  //       }
+  //       form.classList.add('was-validated');
+  //     }, false);
+  //   });
+  // }, false);
 }
