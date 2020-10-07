def994a945f7b8af102916272e5723cd050a3d72
albfelix31 <albert.felix31@gmail.com>
2020-10-07 13:57:10 -0400

    minor adjustments

--- Homework2.js
+++ Homework2.js
@@ -1,51 +1,37 @@
 function displayShippingAddress() {
-    // get shippingAddress
-    var shippingAddress = document.getElementById('shippingAddressBox');
+  // get shippingAddress
+  var shippingAddress = document.getElementById('shippingAddressBox');
 
-    // get the current value of the shipping address div's display property
-    var displaySetting = shippingAddress.style.display;
+  // get the current value of the shipping address div's display property
+  var displaySetting = shippingAddress.style.display;
 
-    // also get the address checkbox value, so we can change what it says
-    var addressCheck = document.getElementById('addressCheck');
+  // also get the address checkbox value, so we can change what it says
+  var addressCheck = document.getElementById('addressCheck');
 
-    // now toggle the clock and the button text, depending on current state
-    if (displaySetting == 'none') {
-      // clock is visible. hide it
-      shippingAddress.style.display = 'block';
-    }
-    else {
-      // clock is hidden. show it
-      shippingAddress.style.display = 'none';
-    }
+  // now toggle the clock and the button text, depending on current state
+  if (displaySetting == 'none') {
+    // clock is visible. hide it
+    shippingAddress.style.display = 'block';
   }
-
-  function validate() {
-    // var msg= "", fields = document.getElementById("checkout").getElementsByTagName("input");
-
-    // for (var i=0; i < fields.length; i++){
-    //   if (fields[i].value == "") 
-    //     msg += fields[i].title + ' is required. \n';
-    // }
-
-    // if(msg) {
-    //   alert(msg);
-    //   return false;
-    // }
-    // else
-    //   return true;
-  //   'use strict';
-  //   window.addEventListener('load', function() {
-  //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  //   var forms = document.getElementById("checkout");
-  //   // Loop over them and prevent submission
-  //   var validation = Array.prototype.filter.call(forms, function(form) {
-  //     form.addEventListener('submit', function(event) {
-  //       if (form.checkValidity() === false) {
-  //         event.preventDefault();
-  //         event.stopPropagation();
-  //       }
-  //       form.classList.add('was-validated');
-  //     }, false);
-  //   });
-  // }, false);
+  else {
+    // clock is hidden. show it
+    shippingAddress.style.display = 'none';
+  }
 }
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})()
