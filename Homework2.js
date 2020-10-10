7d3337d8b334e1847e4612846df4154bc40b5e00
albfelix31 <albert.felix31@gmail.com>
2020-10-09 23:33:21 -0400

    final touchups

--- Homework2.js
+++ Homework2.js
@@ -1,23 +1,19 @@
 function displayShippingAddress() {
-  // get shippingAddress
+  // get the shipping address div
   var shippingAddress = document.getElementById('shippingAddressBox');
 
   // get the current value of the shipping address div's display property
   var displaySetting = shippingAddress.style.display;
 
-  // also get the address checkbox value, so we can change what it says
-  var addressCheck = document.getElementById('addressCheck');
-
-  // now toggle the clock and the button text, depending on current state
+  // display shipping address div depending on current state
   if (displaySetting == 'none') {
-    // clock is visible. hide it
     shippingAddress.style.display = 'block';
   }
   else {
-    // clock is hidden. show it
     shippingAddress.style.display = 'none';
   }
 }
+
 (function() {
   'use strict';
   window.addEventListener('load', function() {
